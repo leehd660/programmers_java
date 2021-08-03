295bcf2819f68ef9700be15b50fd86448c09a20a
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-08-03 19:43:55 +0900

    08.03 평균구하기 문제 해결 - 처음에 int를 int로 나눠서 소수점이 나오지 않아서 당황했었음. 항상 데이터 타입을 생각하기

---
+++ src/Level1/평균구하기.java
@@ -0,0 +1,17 @@
+package Level1;
+
+public class 평균구하기 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4};
+        System.out.println(solution(array));
+    }
+
+    public static double solution(int[] arr) {
+        double sum=0;
+        for (int i=0; i<arr.length; i++) {
+            sum += arr[i];
+        }
+        double answer = sum/arr.length;
+        return answer;
+    }
+}
