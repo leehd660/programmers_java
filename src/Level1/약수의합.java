6a77807e74a416644cf3b882a7c8a039da5e8aac
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-08-07 13:42:39 +0900

    08.07 약수의 합 문제 해결

---
+++ src/Level1/약수의합.java
@@ -0,0 +1,13 @@
+package Level1;
+
+public class 약수의합 {
+    public int solution(int n) {
+        int answer = n;
+        for (int i=1; i<=n/2; i++){
+            if (n%i==0){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
