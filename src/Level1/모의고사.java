ee2d1082c85cfe11ffaddba4fbf227687409ee7c
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-08-18 23:53:33 +0900

    08.18 모의고사 문제 미해결 - 하나는 실패, 하나는 런타임에러

---
+++ src/Level1/모의고사.java
@@ -0,0 +1,131 @@
+package Level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 모의고사 {
+    public int[] solution(int[] answers) {
+        List<Integer> ans = new ArrayList<>();
+        int[] numlist = {0,0,0};
+        int num1 =0;
+        int num2 =0;
+        int num3 =0;
+        for (int i=1; i<answers.length+1; i++) {
+            if (answers[i-1]%5 == i%5){
+                numlist[0]++;
+                num1++;
+            }
+            int num2_1 =0;
+            switch (i%8){
+                case 1: num2_1=2; break;
+                case 2: num2_1=1; break;
+                case 3: num2_1=2; break;
+                case 4: num2_1=3; break;
+                case 5: num2_1=2; break;
+                case 6: num2_1=4; break;
+                case 7: num2_1=2; break;
+                case 0: num2_1=5; break;
+            }
+            if (num2_1 == answers[i-1]){
+                numlist[1]++;
+                num2++;
+            }
+            int num3_1 =0;
+            switch (i%10) {
+                case 1: num3_1=3; break;
+                case 2: num3_1=3; break;
+                case 3: num3_1=1; break;
+                case 4: num3_1=1; break;
+                case 5: num3_1=2; break;
+                case 6: num3_1=2; break;
+                case 7: num3_1=4; break;
+                case 8: num3_1=4; break;
+                case 9: num3_1=5; break;
+                case 0: num3_1=5; break;
+            }
+            if (num3_1==answers[i-1]){
+                numlist[2]++;
+                num3++;
+            }
+        }
+        for (int i=0; i<2; i++){
+            for(int j=i+1; j<3; j++){
+                int big = numlist[i];
+                int small = numlist[j];
+                if(numlist[i]<numlist[j]){
+                    numlist[i] = big;
+                    numlist[j] = small;
+                }
+            }
+        }
+        if(numlist[0] == num1){
+            ans.add(1);
+        }
+        if(numlist[0] == num2){
+            ans.add(2);
+        }
+        if(numlist[0] == num3){
+            ans.add(3);
+        }
+        int[] answer = new int[ans.size()];
+        for (int i=0; i<ans.size(); i++){
+            answer[i]=ans.get(i);
+        }
+        return answer;
+    }
+
+
+
+
+
+
+    public int[] solution1(int[] answers) {
+        List<Integer> ans = new ArrayList<>();
+        int[] numlist = {0,0,0};
+        int[] num2List = {2,1,2,3,2,4,2,5};
+        int[] num3List = {3,3,1,1,2,2,4,4,5,5};
+        int num1 =0;
+        int num2 =0;
+        int num3 =0;
+        for (int i=1; i<answers.length+1; i++) {
+            if (answers[i - 1] % 5 == i % 5) {
+                numlist[0]++;
+                num1++;
+            }
+            if (answers[i - 1] == num2List[(i%8)-1]){
+                numlist[1]++;
+                num2++;
+            }
+            if (answers[i-1] == num3List[(i%10)-1]){
+                numlist[2]++;
+                num3++;
+            }
+        }
+        System.out.println(num1 +"g"+num2 +"g"+num3);
+        for (int i=0; i<2; i++){
+            for(int j=i+1; j<3; j++){
+                int big = numlist[i];
+                int small = numlist[j];
+                if(numlist[i]<numlist[j]){
+                    numlist[i] = big;
+                    numlist[j] = small;
+                }
+            }
+        }
+        System.out.println(ans.get(0) +"g"+ans.get(1) +"g"+ans.get(2));
+        if(numlist[0] == num1){
+            ans.add(1);
+        }
+        if(numlist[0] == num2){
+            ans.add(2);
+        }
+        if(numlist[0] == num3){
+            ans.add(3);
+        }
+        int[] answer = new int[ans.size()];
+        for (int i=0; i<ans.size(); i++){
+            answer[i]=ans.get(i);
+        }
+        return answer;
+    }
+}
