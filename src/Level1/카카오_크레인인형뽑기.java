aac43ce1528e7cd7da6c22afc9980fddbe1a1516
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-08-22 02:43:32 +0900

    08.22 카카오_크레인 인형 뽑기 문제 해결 - 어제는 해결 못했었는데, 알고보니까 삽입 안해도 0으로 만들어야했고 다 사라졌을 때 bucket의 크기역시 생각해야했음

--- src/Level1/카카오_크레인인형뽑기.java
+++ src/Level1/카카오_크레인인형뽑기.java
@@ -6,8 +6,11 @@ import java.util.List;
 public class 카카오_크레인인형뽑기 {
     public static void main(String[] args) {
         int[][] arr = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[][] arr2 = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
         int[] arr1 = {1,5,3,5,1,2,1,4};
-        System.out.println(solution(arr,arr1));
+        int[] arr4 = {1,5,3,5,1,2,5,1,4,3};
+        System.out.println(solution(arr2,arr4));
+        //4311322 45
     }
     public static int solution(int[][] board, int[] moves) {
         int answer = 0;
@@ -28,7 +31,13 @@ public class 카카오_크레인인형뽑기 {
                     else {
                         System.out.println(bucket.get(bucket.size()-1)+"지워지는거");
                         bucket.remove(bucket.size()-1);
-                        beforeNum = bucket.get(bucket.size()-1);
+                        if (bucket.size() != 0){
+                            beforeNum = bucket.get(bucket.size()-1);
+                        }
+                        else {
+                            beforeNum = 0;
+                        }
+                        board[j][moves[i] - 1] = 0;
                         answer +=2;
                         break;
                     }
